a0682d55fdc1a8111da89c8763a3bdaa235cfaa5
Mina S. Aziz <minaz922@gmail.com>
2017-03-31 13:56:30 +0200

    add new method to return Hero obj by id

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -15,4 +15,9 @@ export class HeroService {
       setTimeout(() => resolve(HEROES), 2000);
     });
   }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+      .then(heroes => heroes.find(hero => hero.id === id));
+  }
 }
