5be120a067aa0994ef8809ced6f82829f219fb99
Mina S. Aziz <minaz922@gmail.com>
2017-03-31 13:57:02 +0200

    add mini-view for hero details with ability to navigate to HeroDetailComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -15,7 +16,9 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {
+  constructor(
+    private router: Router,
+    private heroService: HeroService) {
 
   }
 
@@ -29,5 +32,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
